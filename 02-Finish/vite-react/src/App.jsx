bcd8ca3f51bfd6a3423d98d6cf54bf0c4c4943b6
Josh Taylor <thie.common.coder@gmail.com>
2025-11-04 17:33:08 -0800

    feat: add tailwind to react vite demo, update styling

--- 02-Finish/vite-react/src/App.jsx
+++ 02-Finish/vite-react/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div class="h-full bg-slate-800 text-white flex flex-col items-center justify-center">
+      <div class="flex">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +16,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 class="text-5xl font-bold my-5">Vite + React</h1>
+      <div class="flex flex-col">
+        <button class="bg-gray-500 py-2 px-2 rounded text-white cursor-pointer" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p class="my-5">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
